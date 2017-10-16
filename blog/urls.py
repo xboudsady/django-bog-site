e4df04b0dbb013a016754e89a28c53f7f118c1a1
Xayaseth Boudsady <boudsadyx@gmail.com>
2017-10-15 18:35:06 -0700

    Created urls.py file, added urlpattern dictionary

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # post views
+    url(r'^$', views.post_list, name='post_list'),
+    # url(r'^$', views.PostListView.as_view(), name='post_list'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\W]+)/$',
+        views.post_detail,
+        name = 'post_detail'),
+]
