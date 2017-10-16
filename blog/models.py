709c12bce1e61f718652499f16daf33646002c06
Xayaseth Boudsady <boudsadyx@gmail.com>
2017-10-15 18:18:02 -0700

    Updated Models.py to include models.Manager() and published

--- blog/models.py
+++ blog/models.py
@@ -24,6 +24,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # The Dahl-specific manager.
+
     class Meta:
         ordering = ('-publish',)
 
